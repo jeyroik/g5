312170f8f9eac44c62a429a84ca350db38274ea1
Aleksandr Ivanov <aivanov@hq.fix.ru>
2018-06-12 15:14:55 +0300

    dist upd

--- resources/configs/container.dist.php
+++ resources/configs/container.dist.php
@@ -2,9 +2,10 @@
 
 use tratabor\interfaces\systems\states as IStates;
 use tratabor\components\systems\states as CStates;
+use tratabor\interfaces\systems\plugins as ISystemPlugins;
 
 return [
     IStates\IStateFactory::class => CStates\StateFactory::class,
     IStates\dispatchers\IDispatchersFactory::class => CStates\dispatchers\DispatcherFactory::class,
-    IStates\machines\streams\IStreamFactory::class => CStates\machines\streams\StreamFactory::class,
+    ISystemPlugins\IPluginRepository::class => \tratabor\components\systems\plugins\PluginRepository::class
 ];
