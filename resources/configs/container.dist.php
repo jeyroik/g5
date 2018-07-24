fec3a43a0ae71eb62ecb67f14f5803682343875d
Aleksandr Ivanov <aivanov@hq.fix.ru>
2018-07-24 18:25:43 +0300

    upd scnt

--- resources/configs/container.dist.php
+++ resources/configs/container.dist.php
@@ -4,6 +4,8 @@ use jeyroik\extas\interfaces\systems as S;
 use jeyroik\extas\interfaces\systems\states as IStates;
 use jeyroik\extas\components\systems\states as CStates;
 use jeyroik\extas\interfaces\systems\plugins as ISystemPlugins;
+use tratabor\interfaces\basics as IBasics;
+use tratabor\components\basics as Basics;
 
 return [
     IStates\IStateFactory::class => CStates\StateFactory::class,
@@ -11,5 +13,6 @@ return [
     ISystemPlugins\IPluginRepository::class => \jeyroik\extas\components\systems\plugins\PluginRepository::class,
     S\extensions\IExtensionRepository::class => \jeyroik\extas\components\systems\extensions\ExtensionRepository::class,
     S\packages\IPackageRepository::class => \jeyroik\extas\components\systems\packages\PackageRepository::class,
-    S\plugins\stages\IStageRepository::class => \jeyroik\extas\components\systems\plugins\stages\StageRepository::class
+    S\plugins\stages\IStageRepository::class => \jeyroik\extas\components\systems\plugins\stages\StageRepository::class,
+    IBasics\users\profiles\IProfileRepository::class => Basics\users\profiles\ProfileRepository::class
 ];
--- src/components/basics/users/profiles/ProfileRepository.php
+++ src/components/basics/users/profiles/ProfileRepository.php
@@ -3,6 +3,7 @@ namespace tratabor\components\basics\users\profiles;
 
 use tratabor\components\basics\users\UserProfile;
 use jeyroik\extas\components\systems\repositories\RepositoryMongo;
+use tratabor\interfaces\basics\users\profiles\IProfileRepository;
 
 /**
  * Class ProfileRepository
@@ -10,7 +11,7 @@ use jeyroik\extas\components\systems\repositories\RepositoryMongo;
  * @package tratabor\components\basics\users\profiles
  * @author Funcraft <me@funcraft.ru>
  */
-class ProfileRepository extends RepositoryMongo
+class ProfileRepository extends RepositoryMongo implements IProfileRepository
 {
     protected $collectionName = 'g5__user_profiles';
     protected $itemClass = UserProfile::class;
